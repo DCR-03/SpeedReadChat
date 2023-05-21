9b78eecf195fbe78eb6cb98bbb4ab904a7455055
Russell Chien <RussellChien6@gmail.com>
2023-05-20 22:46:36 -0700

    finish initial landing page and draft flashcards

--- frontend/src/Flashcard.js
+++ frontend/src/Flashcard.js
@@ -1,12 +1,23 @@
 import React from 'react';
+import './Flashcard.css';
 
-function Flashcard({ text }) {
+function Flashcard() {
   // Implement your flashcard generation logic here based on the text passed to the component
   // You can use the text value to generate and display a flashcard
+  console.log('display flashcards')
+  const data = [
+    { text: 'Flashcard 1' },
+    { text: 'Flashcard 2' },
+    { text: 'Flashcard 3' },
+  ];
 
   return (
     <div>
-      {/* Display the generated flashcard here */}
+      {data.map((card, index) => (
+        <div key={index} className="flashcard">
+          {card.text}
+        </div>
+      ))}
     </div>
   );
 }
